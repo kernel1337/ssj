392c63d838d53a050902cebee85e37a7ac5003e3
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-10-15 19:50:07 +0530

    Update ssj.sh

--- ssj.sh
+++ ssj.sh
@@ -22,9 +22,9 @@ docker build -t ssj . && \
 wget https://raw.githubusercontent.com/thirdbyte/demon-docker/master/root/.bashrc && \
 mkdir /home/ssj && \
 cp .bashrc /home/ssj/.bashrc && \
-wget https://gist.githubusercontent.com/thirdbyte/3b4596ae93c09b3fd950d35329f203c2/raw/ef9cc873b6d0e5c440664851387d612ae1837e80/ssj.desktop && \
-wget https://user-images.githubusercontent.com/59290972/96023362-f173f800-0e6f-11eb-8567-769e194beaec.png && \
-cp *.desktop /usr/local/share/applications/ssj.desktop && \
-cp *png /usr/local/share/applications/ssj.png && \
+wget https://raw.githubusercontent.com/thirdbyte/ssj/main/ssj.desktop && \
+wget https://raw.githubusercontent.com/thirdbyte/ssj/main/ssj.png && \
+cp ssj.desktop /usr/local/share/applications/ssj.desktop && \
+cp ssj.png /usr/local/share/applications/ssj.png && \
 cd /tmp && \
 rm -rf /tmp/ssj
