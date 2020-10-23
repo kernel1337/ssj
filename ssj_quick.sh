0f42806c87b313207ebc0728b0f216419ac4a60f
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-10-23 11:29:42 +0530

    Update ssj_quick.sh

--- ssj_quick.sh
+++ ssj_quick.sh
@@ -17,9 +17,9 @@ fi
 
 mkdir -p /tmp/ssj && \
 cd /tmp/ssj && \
-docker pull scarfaced/demon-docker:latest && \
-docker tag scarfaced/demon-docker:latest ssj:latest && \
-docker image rm scarfaced/demon-docker:latest && \
+docker pull scarfaced/ssj:latest && \
+docker tag scarfaced/ssj:latest ssj:latest && \
+docker image rm scarfaced/ssj:latest && \
 wget https://raw.githubusercontent.com/thirdbyte/ssj/main/.bashrc && \
 mkdir -p /home/ssj && \
 cp .bashrc /home/ssj/.bashrc && \
