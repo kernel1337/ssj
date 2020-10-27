53230e79ba6b9c8387f77d93ff2bc84c5aa66682
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-10-27 15:46:11 +0530

    Update ssj.sh

--- ssj.sh
+++ ssj.sh
@@ -20,7 +20,7 @@ cd /tmp/ssj && \
 docker pull scarfaced/ssj:latest && \
 docker tag scarfaced/ssj:latest ssj:latest
 
-docker image rm $(docker images -a -q --filter "dangling=true") &>/dev/null
+docker image rm $(docker images -q --filter "dangling=true") &>/dev/null
 
 wget https://raw.githubusercontent.com/thirdbyte/ssj/main/.bashrc && \
 mkdir -p /home/ssj && \
