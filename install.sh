98f9ef3d2393e0717f260322f8335e5c509b370d
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-10-31 01:38:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 
 mkdir -p /tmp/ssj && \
 cd /tmp/ssj && \
-docker pull scarfaced/ssj:latest && \
+docker pull scarfaced/ssj:latest
 
 docker image rm $(docker images -q --filter "dangling=true") &>/dev/null
 
