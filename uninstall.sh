2ccebccc6d8d1ff48611850821147c5e409473ae
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-11-09 13:31:37 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,7 +8,7 @@ fi
 docker image rm scarfaced/ssj:latest &>/dev/null
 docker image rm ssj:latest &>/dev/null
 docker image rm $(docker images -q --filter "dangling=true") &>/dev/null
-rm -rf /home/ssj &>/dev/null
-rm -rf /usr/local/bin/ssj &>/dev/null
-rm -rf /usr/local/share/applications/ssj.* &>/dev/null
+rm -rf $HOME/.ssj &>/dev/null
+rm -rf $HOME/.local/bin/ssj &>/dev/null
+rm -rf $HOME/.local/share/applications/ssj.* &>/dev/null
 rm -rf /tmp/ssj &>/dev/null
